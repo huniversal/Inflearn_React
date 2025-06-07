32fb2a2053f07e7c75d5e1d31162381a06310079
huniversal <hunivers0523@naver.com>
2025-06-08 00:48:06 +0900

    feat: 콜백함수

---
+++ section01/chapter13.js
@@ -0,0 +1,24 @@
+// 콜백 함수란?
+// 자신이 아닌 다른 함수에, 인수로써 전달된 함수를 의미 함
+//===========================================================
+// 1. 콜백함수
+// function main(value) {
+//   value();
+// }
+
+// function sub() {
+//   console.log("i am sub");
+// }
+
+// main(sub)
+
+// 2. 콜백함수의 활용 
+function repeat(count, callback) {
+  for(let idx=1; idx<=count; idx++) {
+    callback(idx);
+  }
+}
+
+repeat(5, function (idx){
+  console.log(idx*2)
+})
