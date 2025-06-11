8fdb55922193cb979ced5f40a1c2c2602ef7ca42
huniversal <hunivers0523@naver.com>
2025-06-12 01:32:01 +0900

    feat: 업데이트 시점에만 useEffect 실행

--- lifecycle/src/AppIn.tsx
+++ lifecycle/src/AppIn.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Controller from "./components/Controller";
 import Viewer from "./components/Viewer";
 
@@ -6,9 +6,22 @@ function AppIn() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
 
-  useEffect(() => {
-    console.log(`count: ${count} / ${input}`)
-  }, [count, input])
+  const isMount = useRef(false);
+
+  // 1. 마운트 : 탄생
+  useEffect(()=>{
+    console.log("Mount")
+  },[])
+  
+  // 2. 업데이트 : 변화, 리렌더링
+  useEffect(()=>{
+    if(!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("Update");
+  })
+  // 3. 언마운트 : 죽음
 
   const onClickButton = (value: number) => {
     setCount((prev) => prev + value);
