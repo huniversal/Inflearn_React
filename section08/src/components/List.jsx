0c7faf439e01566e10dda08604d96069ca9ad681
huniversal <hunivers0523@naver.com>
2025-06-15 20:12:52 +0900

    feat: todolist 삭제

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import TodoItem from "./TodoItem";
 import "./List.css"
 
-const List = ({todos, onUpdate}) => {
+const List = ({todos, onUpdate, onDelete}) => {
   const [search, setSearch] = useState("");
 
   const onChangeSearch = (e) => {
@@ -35,6 +35,7 @@ const List = ({todos, onUpdate}) => {
             <TodoItem
               key={todo.id} {...todo} 
               onUpdate={onUpdate}
+              onDelete={onDelete}
             />
           )
         })}
