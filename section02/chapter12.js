21513752363d45e6a8d5c2d81d03b8df4465e976
huniversal <hunivers0523@naver.com>
2025-06-24 01:42:23 +0900

    feat: 비동기 작업 callback 예제 테스트

--- section02/chapter12.js
+++ section02/chapter12.js
@@ -1,10 +1,43 @@
-function add(a, b, callback) {
+// function add(a, b, callback) {
+//   setTimeout(() => {
+//     const sum = a + b;
+//     callback(sum);
+//   }, 3000);
+// }
+
+// add(1, 2, (value) => {
+//   console.log(value);
+// });
+
+function orderFood(callback) {
   setTimeout(() => {
-    const sum = a + b;
-    callback(sum);
-  }, 3000);
+    const food = 'Pizza';
+    callback(food);
+  }, 3000)
 }
 
-add(1, 2, (value) => {
-  console.log(value);
+function cooldownFood(food) {
+  setTimeout(() => {
+    const cooldownFood = `식은 ${food}`;
+    callback(cooldownFood);
+  }, 2000);
+}
+
+function frreezeFood(food, callback) {
+  setTimeout(() => {
+    const frreezedFood = `얼린 ${food}`;
+    callback(frreezedFood);
+  }, 3000)
+}
+
+orderFood((food) => {
+  console.log(food);
+
+  cooldownFood(food, (cooldownFood) => {
+    console.log(cooldownFood);
+
+    frreezeFood(cooldownFood, (frreezedFood) => {
+      console.log(frreezedFood);
+    });
+  });
 });
