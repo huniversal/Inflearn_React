f1bb6d34f4519bc3859b282fcd158ad132428650
huniversal <hunivers0523@naver.com>
2025-06-26 00:30:29 +0900

    feat: React Redux 상태관리 기초 세팅

---
+++ section_redux/src/App.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import "./App.css";
+import AddNumberRoot from './components/AddNumberRoot';
+import DisplayNumberRoot from './components/DisplayNumberRoot';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Root</h1>
+      <AddNumberRoot></AddNumberRoot>
+      <DisplayNumberRoot></DisplayNumberRoot>
+    </div>
+  )
+}
+
+
+
+
+
+
+export default App
