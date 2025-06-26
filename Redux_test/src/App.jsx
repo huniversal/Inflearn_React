d870f4441b1f6a68794973711f9f007eff052f0f
huniversal <hunivers0523@naver.com>
2025-06-27 01:56:02 +0900

    feat: payload 테스트

--- Redux_test/src/App.jsx
+++ Redux_test/src/App.jsx
@@ -6,14 +6,14 @@ function App() {
   const count = useSelector(state => state.count)
   const dispatch = useDispatch();
   const increase = () => {
-    dispatch({type: "INCREMENT"})
+    dispatch({type: "INCREMENT", payload: {num: 5}})
   }
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={increase}>중가</button>
       <Box />
-      
+
     </div>
   )
 }
--- Redux_test/src/redux/reducer/reducer.js
+++ Redux_test/src/redux/reducer/reducer.js
@@ -6,7 +6,7 @@ function reducer(state = initialState, action) {
   console.log(action)
   switch (action.type) {
     case "INCREMENT": 
-      return { ...state, count: state.count + 1 };
+      return { ...state, count: state.count + action.payload.num };
     default:
       return { ...state }
   }
