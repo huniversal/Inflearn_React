92b11fa200c2d29a6f4fef431ce94ff2f2179719
huniversal <hunivers0523@naver.com>
2025-06-27 01:59:50 +0900

    feat: 간단한 예제를 통한 payload 테스트

--- Redux_test/src/App.jsx
+++ Redux_test/src/App.jsx
@@ -4,14 +4,23 @@ import Box from "./components/Box.jsx"
 
 function App() {
   const count = useSelector(state => state.count)
+  const id = useSelector(state => state.id)
+  const password = useSelector(state => state.password)
   const dispatch = useDispatch();
   const increase = () => {
     dispatch({type: "INCREMENT", payload: {num: 5}})
   }
+
+  const login = () => {
+    dispatch({type: "LOGIN", payload: {id: "hunjin", password: "123"}})
+  }
   return (
     <div>
       <h1>{count}</h1>
+      <h1>{id}</h1>
+      <h1>{password}</h1>
       <button onClick={increase}>중가</button>
+      <button onClick={login}>로그인</button>
       <Box />
 
     </div>
