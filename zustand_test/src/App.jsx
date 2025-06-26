f088ed612880cf6349b4bf08388fe0b50d570339
huniversal <hunivers0523@naver.com>
2025-06-27 02:28:22 +0900

    feat: 매개 변수를 통한 store 함수 추가

--- zustand_test/src/App.jsx
+++ zustand_test/src/App.jsx
@@ -4,11 +4,12 @@ import CountBox from './components/countBox';
 import counterStore from './stores/counterStore';
 
 function App() {
-  const {count, increase} = counterStore();
+  const {count, increase, increaseBy} = counterStore();
   return (
     <div>
       <h1>count: {count}</h1>
       <button onClick={increase}>증가</button>
+      <button onClick={() => increaseBy(10)}>10씩 증가</button>
       <CountBox />
     </div>
   )
