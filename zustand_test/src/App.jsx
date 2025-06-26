ced007c23eda1c7eda3d0d1cc53952a483f6fe59
huniversal <hunivers0523@naver.com>
2025-06-27 02:20:34 +0900

    feat: zustand를 통해 store의 값을 받아옴

--- zustand_test/src/App.jsx
+++ zustand_test/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useState } from "react";
+import CountBox from './components/countBox';
+import counterStore from './stores/counterStore';
 
 function App() {
+  const {count} = counterStore();
   return (
     <div>
-      hi
+      <h1>count: {count}</h1>
+      <CountBox count={count}/>
     </div>
   )
 }
---
+++ zustand_test/src/components/CountBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CountBox = ({count}) => {
+  return (
+    <div>
+      countBox : {count}
+    </div>
+  )
+}
+
+export default CountBox
