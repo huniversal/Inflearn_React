204ace2c4d29da13cdbad07b24662cb6ffd88fc8
huniversal <hunivers0523@naver.com>
2025-06-28 21:52:14 +0900

    feat: Form 추가 store 함수 작성

--- zustand_simple_project/src/components/ContactForm.jsx
+++ zustand_simple_project/src/components/ContactForm.jsx
@@ -1,9 +1,19 @@
 import React, { useState } from 'react'
 import { Box, TextField, Button } from "@mui/material"
+import usePhoneBookStore from '../stores/usePhoneBook';
+
 
 const ContactForm = () => {
   const [name, setName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
+  const {addContact} = usePhoneBookStore();
+
+  const handleAddContact = () => {
+    // 연락처 저장 장소(배열 : phoneBook = [])
+    // 연락처를 추가
+    if(name.trim () || phoneNumber.trim()) return 
+    addContact(name, phoneNumber);
+  }
   return (
     <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
       <TextField 
@@ -20,7 +30,10 @@ const ContactForm = () => {
         value={phoneNumber}
         onChange={(e) => setPhoneNumber(e.target.value)}
       />
-      <Button variant="contained" size='large'>추가</Button>
+      <Button 
+        variant="contained" 
+        size='large' 
+        onClick={handleAddContact}>추가</Button>
     </Box>
   )
 }
---
+++ zustand_simple_project/src/stores/usePhoneBook.js
@@ -0,0 +1,12 @@
+import e from "express";
+import {create} from "zustand";
+
+const usePhoneBookStore = create((set) => ({
+  phoneBook: [], 
+  addContact: (name, phoneNumber) => 
+    set((state) => ({
+      phoneBook: [...state.phoneBook, {id:Date.now(), name, phoneNumber}],
+    })),
+}));
+
+export default usePhoneBookStore;
